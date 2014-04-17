7aa3d8e1f718839635db8ea05446c30d05020d51
Julien Bourdeau <julien@sigerr.org>
2014-04-17 12:15:01 +0200

    // Version compliancy

--- followup.php
+++ followup.php
@@ -33,7 +33,7 @@ class Followup extends Module
 	{
 		$this->name = 'followup';
 		$this->tab = 'advertising_marketing';
-		$this->version = '1.6';
+		$this->version = '1.6.1';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
@@ -65,7 +65,6 @@ class Followup extends Module
 		$this->displayName = $this->l('Customer follow-up');
 		$this->description = $this->l('Follow-up with your customers with daily customized e-mails.');
 		$this->confirmUninstall = $this->l('Are you sure you want to delete all settings and your logs?');
-		$this->ps_versions_compliancy = array('min' => '1.5.6.1', 'max' => _PS_VERSION_);
 	}
 
 	public function install()
